914852e561a08569b917ac3f438687996b37cfb1
manish6625 <168397631+manish6625@users.noreply.github.com>
2025-11-09 22:14:33 +0530

    Refactor Fibonacci method for clarity

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,10 +7,9 @@ public class Fibonacci {
 		if(n==0) {
 			return 0;
 		}
-		else if(n==1) {
+		if(n==1) {
 			return 1;
 		}
-		else{
 			return fibonacci(n-1)+fibonacci(n-2);
 		}
 	}
@@ -24,3 +23,4 @@ public class Fibonacci {
 	}
 
 }
+
